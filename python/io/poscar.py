d3672b2dc03cb821bf2e6b9a7c1a40ba2b5d5f79
Le Van Hung <lvhungqt@gmail.com>
2026-07-19 21:08:00 +0700

    Add structural properties to Poscar class

--- python/io/poscar.py
+++ python/io/poscar.py
@@ -6,6 +6,7 @@ POSCAR I/O
 """
 
 from pathlib import Path
+import math
 
 
 class PoscarError(Exception):
@@ -40,6 +41,86 @@ class Poscar:
     @property
     def natoms(self):
         return sum(self.counts)
+
+    # -----------------------------------------------------
+
+    @property
+    def species(self):
+        return list(self.elements)
+
+    # -----------------------------------------------------
+
+    @property
+    def formula(self):
+
+        out = []
+
+        for e, n in zip(self.elements, self.counts):
+
+            out.append(e)
+
+            if n != 1:
+                out.append(str(n))
+
+        return "".join(out)
+
+    # -----------------------------------------------------
+
+    @property
+    def is_direct(self):
+
+        return self.coordinate_type.lower().startswith("d")
+
+    # -----------------------------------------------------
+
+    @property
+    def is_cartesian(self):
+
+        return self.coordinate_type.lower().startswith("c")
+
+    # -----------------------------------------------------
+
+    @property
+    def volume(self):
+
+        a = self.lattice[0]
+        b = self.lattice[1]
+        c = self.lattice[2]
+
+        return abs(
+            a[0]*(b[1]*c[2]-b[2]*c[1])
+          - a[1]*(b[0]*c[2]-b[2]*c[0])
+          + a[2]*(b[0]*c[1]-b[1]*c[0])
+        ) * self.scale**3
+
+    # -----------------------------------------------------
+
+    @property
+    def reciprocal_lattice(self):
+
+        a = [x*self.scale for x in self.lattice[0]]
+        b = [x*self.scale for x in self.lattice[1]]
+        c = [x*self.scale for x in self.lattice[2]]
+
+        def cross(u,v):
+            return [
+                u[1]*v[2]-u[2]*v[1],
+                u[2]*v[0]-u[0]*v[2],
+                u[0]*v[1]-u[1]*v[0],
+            ]
+
+        def dot(u,v):
+            return sum(i*j for i,j in zip(u,v))
+
+        v = dot(a,cross(b,c))
+
+        factor = 2*math.pi/v
+
+        b1 = [factor*x for x in cross(b,c)]
+        b2 = [factor*x for x in cross(c,a)]
+        b3 = [factor*x for x in cross(a,b)]
+
+        return [b1,b2,b3]
 
     # -----------------------------------------------------
 
