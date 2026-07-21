3e55cfb68ee6998f85bdb0e764e8a141859da1c8
Le Van Hung <lvhungqt@gmail.com>
2026-07-21 08:10:17 +0700

    Add numerical regression tests for VASP parsers

--- tests/test_eigenval.py
+++ tests/test_eigenval.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 from python import EIGENVALParser
 
 DATA = Path(__file__).parent / "data" / "gaas" / "EIGENVAL"
@@ -9,8 +11,17 @@ def test_eigenval():
 
     p = EIGENVALParser().read(DATA).parse()
 
+    assert p.nelect == 8
     assert p.nkpts == 455
     assert p.nbands == 8
 
     assert p.eigenvalues.shape == (455, 8)
     assert p.occupations.shape == (455, 8)
+    assert p.kpoints.shape == (455, 3)
+    assert p.weights.shape == (455,)
+
+    assert p.eigenvalues[0,0] == pytest.approx(-10.023773)
+    assert p.eigenvalues[0,-1] == pytest.approx(6.032472)
+
+    assert p.occupations[0,0] == pytest.approx(1.0)
+    assert p.weights[0] == pytest.approx(6.4e-05)
--- tests/test_procar.py
+++ tests/test_procar.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 from python import PROCARParser
 
 DATA = Path(__file__).parent / "data" / "gaas" / "PROCAR"
@@ -15,5 +17,12 @@ def test_procar():
 
     assert p.band_energy.shape == (455, 8)
     assert p.band_occ.shape == (455, 8)
-
+    assert p.kpoints.shape == (455, 3)
+    assert p.weights.shape == (455,)
     assert p.projections.shape == (455, 8, 2, 10)
+
+    assert p.band_energy[0,0] == pytest.approx(-10.02377286)
+    assert p.band_occ[0,0] == pytest.approx(2.0)
+    assert p.projections[0,0,0,0] == pytest.approx(0.234)
+
+    assert p.weights[0] == pytest.approx(6.4e-05)
