8b998271487b2cd7f5775aeec65f4fae6224093a
Le Van Hung <lvhungqt@gmail.com>
2026-07-22 11:00:25 +0700

    Fix import test after package rename

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,109 +1,16 @@
 """
-Basic import tests.
+Basic import tests for dft_toolkit.
 """
 
-import python
+import dft_toolkit
 
 
 def test_package_import():
-
-    assert python is not None
-
-
-def test_procar():
-
-    from python import PROCARParser
-
-    assert PROCARParser is not None
+    """Package can be imported."""
+    assert dft_toolkit is not None
 
 
-def test_eigenval():
-
-    from python import EIGENVALParser
-
-    assert EIGENVALParser is not None
-
-
-def test_doscar():
-
-    from python import DOSCARParser
-
-    assert DOSCARParser is not None
-
-
-def test_outcar():
-
-    from python import OUTCARParser
-
-    assert OUTCARParser is not None
-
-def test_xdatcar():
-    from python import XDATCARParser
-
-def test_chgcar():
-    from python import CHGCARParser
-
-def test_locpot():
-    from python import LOCPOTParser
-
-def test_work_function():
-    from python import WorkFunctionAnalyzer
-
-def test_bandstructure():
-    from python import BandStructure
-
-def test_effective_mass():
-    from python import EffectiveMassAnalyzer
-
-def test_charge_difference():
-    from python import ChargeDifferenceAnalyzer
-
-def test_density_profile():
-    from python import DensityProfileAnalyzer
-
-def test_profile_writer():
-    from python import ProfileWriter
-
-def test_bandstructure_writer():
-    from python import BandStructureWriter
-
-def test_kpath():
-    from python import KPath
-
-def test_dos():
-    from python import DOS
-
-def test_bandgap_analyzer():
-    from python import BandGapAnalyzer
-
-def test_bandgap_result():
-    from python import BandGapResult
-
-def test_electronic_structure_report():
-    from python import ElectronicStructureReport
-
-def test_json_report_writer():
-    from python import JSONReportWriter
-
-def test_csv_report_writer():
-    from python import CSVReportWriter
-
-def test_incar():
-    from python import INCARParser
-
-def test_kpoints_parser():
-    from python import KPOINTSParser
-
-def test_vasprun():
-    from python import VASPRUNParser
-
-
-def test_cli():
-    from python.cli import main
-
-def test_exceptions():
-    from python import (
-        DFTToolkitError,
-        ParserError,
-        InvalidFileError,
-    )
+def test_version_exists():
+    """Package defines a version string."""
+    assert hasattr(dft_toolkit, "__version__")
+    assert isinstance(dft_toolkit.__version__, str)
