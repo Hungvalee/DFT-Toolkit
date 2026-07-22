0f9e75f571c4704723e5de9a59511521eb43f9f0
Le Van Hung <lvhungqt@gmail.com>
2026-07-22 07:10:14 +0700

    Add functional command-line interface

--- python/cli/main.py
+++ python/cli/main.py
@@ -1,24 +1,59 @@
 import argparse
 
-from python import __version__
+from python.parsers import OUTCARParser
+from python.analysis import BandGapAnalyzer
+
+
+def cmd_info(args):
+    parser = OUTCARParser().read(args.file)
+    data = parser.parse()
+    print(data)
+
+
+def cmd_bandgap(args):
+    analyzer = BandGapAnalyzer(args.file)
+    result = analyzer.analyze()
+    print(result)
 
 
 def main():
+
     parser = argparse.ArgumentParser(
         prog="dft-toolkit",
-        description="DFT-Toolkit: A Python toolkit for VASP analysis"
+        description="DFT-Toolkit command-line interface"
     )
 
-    parser.add_argument(
-        "--version",
-        action="version",
-        version=f"%(prog)s {__version__}"
+    subparsers = parser.add_subparsers(dest="command")
+
+    info = subparsers.add_parser(
+        "info",
+        help="Read OUTCAR information"
+    )
+    info.add_argument("file")
+    info.set_defaults(func=cmd_info)
+
+    bandgap = subparsers.add_parser(
+        "bandgap",
+        help="Calculate band gap"
+    )
+    bandgap.add_argument("file")
+    bandgap.set_defaults(func=cmd_bandgap)
+
+    version = subparsers.add_parser(
+        "version",
+        help="Show version"
     )
 
-    parser.parse_args()
+    args = parser.parse_args()
 
-    print("DFT-Toolkit")
-    print("Use --help to see available commands.")
+    if args.command == "version":
+        print("DFT-Toolkit v1.0.0-dev")
+        return
+
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
